fc5ae3ddaf66cfbde69a15b670f19b81fad0167e
Rodrigo Navas <rodrigo-navas@outlook.com>
2021-10-04 08:19:28 -0300

    Subindo projeto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using mvc1.Models;
+using System.Diagnostics;
+
+namespace mvc1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IRepository _repository;
+        private string message;
+
+        public HomeController(ILogger<HomeController> logger, IRepository repository, IConfiguration configuration)
+        {
+            _logger = logger;
+            _repository = repository;
+            message = $"Docker - ({configuration["HOSTNAME"]})";
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = message;
+            return View(_repository.Produtos);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ProdutosRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace mvc1.Models
+{
+    public class ProdutosRepository : IRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public ProdutosRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Produto> Produtos => _appDbContext.Produtos;
+    }
+}
